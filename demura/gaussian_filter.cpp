0b638d249a5e79be0cc7c9c63ba6563e40d5057e
gmy <21821055@zju.edu.cn>
2020-02-27 21:11:02 +0800

    update
    修按顺序找到各个中心点&高斯

--- demura/gaussian_filter.cpp
+++ demura/gaussian_filter.cpp
@@ -8,9 +8,24 @@ using namespace Eigen;
 
 extern Mat img;
 
+double gauss_sample(const int size, const double sigma,
+	const int x, const int y, const int center)
+{
+	double v=0, range= 0.5, interval = 0.1;
+	for (double yi = y - range; yi < y + range; yi += interval)
+	{
+		for (double xi = x - range; xi < x + range; xi += interval)
+		{
+			v += (1 / (2 * PI*sigma*sigma))*exp(-((xi - center)*(xi - center)
+				+ (yi - center)*(yi - center)) / (2 * sigma*sigma));
+		}
+	}
+	return v / (interval * 2 / interval);
+}
+
 void get_gaussian_kernel(vector<vector<double>>& gaus, const int size, const double sigma)
 {
-	if (size==0 || gaus.size() != size || gaus[0].size() != size)
+	if (size==0 || gaus.size() != size+2 || gaus[0].size() != size+2)
 	{
 		cout << "get_gaussian_kernel(): the para not correct!" << endl;
 		return;
@@ -21,14 +36,14 @@ void get_gaussian_kernel(vector<vector<double>>& gaus, const int size, const dou
 	{
 		for (int j = 0; j < size; j++)
 		{
-			gaus[i][j] = (1 / (2 * PI*sigma*sigma))*exp(-((i - center)*(i - center) + (j - center)*(j - center)) / (2 * sigma*sigma));
-			sum += gaus[i][j];
+			//gaus[i+1][j+1] = (1 / (2 * PI*sigma*sigma))*exp(-((i - center)*(i - center) + (j - center)*(j - center)) / (2 * sigma*sigma));
+			gaus[i + 1][j + 1] = gauss_sample(size, sigma, i, j, center);
+			sum += gaus[i+1][j+1];
 		}
 	}
-
-	for (int i = 0; i < size; i++)
+	for (int i = 1; i < size+1; i++)
 	{
-		for (int j = 0; j < size; j++)
+		for (int j = 1; j < size+1; j++)
 		{
 			gaus[i][j] /= sum;
 		}
@@ -36,10 +51,9 @@ void get_gaussian_kernel(vector<vector<double>>& gaus, const int size, const dou
 	return;
 }
 
-const int half_kernel_size = 1;  // 3*3, half is 1
-void get_region_kernels(vector<VectorXd>& kernels, const int size, const int sigma)
+void get_region_kernels(vector<VectorXd>& kernels, const int size, const double sigma)
 {
-	vector<vector<double>> gaus(size, vector<double>(size));
+	vector<vector<double>> gaus(size+2, vector<double>(size+2));
 	get_gaussian_kernel(gaus, size, sigma);
 	/*for (auto g : gaus)
 	{
@@ -49,27 +63,55 @@ void get_region_kernels(vector<VectorXd>& kernels, const int size, const int sig
 		}
 		cout << endl;
 	}*/
-	int total_kernels_cnt = size - 2 * half_kernel_size;
-	kernels.resize(total_kernels_cnt*total_kernels_cnt, VectorXd(9));
-	for (int i = half_kernel_size; i < size - half_kernel_size; i++)
+	kernels.resize(size*size, VectorXd(9));
+	for (int i = 1; i < size + 1; i++)
 	{
-		for (int j = half_kernel_size; j < size - half_kernel_size; j++)
+		for (int j = 1; j < size + 1; j++)
 		{
-			kernels[(i - half_kernel_size)*total_kernels_cnt + j - half_kernel_size] <<
+			kernels[(i-1)*size + j-1] <<
 				gaus[i - 1][j - 1], gaus[i - 1][j], gaus[i - 1][j + 1],
 				gaus[i][j - 1], gaus[i][j], gaus[i][j + 1],
 				gaus[i + 1][j - 1], gaus[i + 1][j], gaus[i + 1][j + 1];
+			kernels[(i - 1)*size + j - 1].normalize();
 		}
 	}
 	//cout << kernels[0];
 }
 
-void compute_sigma(const vector<VectorXd>& centers_vec,const int size, const int sigma)
+void combination_kernels(const vector<VectorXd>& kernels)
+{
+	/*cout << omp_get_num_procs() << endl;
+	omp_set_num_threads(12);*/
+	int size = sqrt(kernels.size()), kernel_size = 3;
+	cout << "kernels count: " << size << endl;
+	Mat k(Size(size * kernel_size, size * kernel_size), CV_8UC1);
+//#pragma parallel omp for
+	for (int y = 0; y < size; y++)
+	{
+		for (int x = 0; x < size; x++)
+		{
+			for (int yy = 0; yy < kernel_size; yy++)
+			{
+				for (int xx = 0; xx < kernel_size; xx++)
+				{
+					/*cout << x << " " << " " << yy << " " << x * kernel_size + xx << ","
+						<< xx << " " << yy << " " << x << " " << yy * kernel_size + xx << endl;*/
+					k.at<byte>(y * kernel_size + yy, x * kernel_size + xx)
+						= (byte)(kernels[y*size + x][yy*kernel_size + xx] *255);
+				}
+			}
+		}
+	}
+	imwrite("./output/kernels.png", k);
+}
+
+void compute_sigma(const vector<VectorXd>& centers_vec,const int size, const double sigma)
 {
 	//int size = 16, sigma = 10;
 	vector<VectorXd> kernels;
 	Performance p;
 	get_region_kernels(kernels, size, sigma);
+	//combination_kernels(kernels);
 	cout << "get_kernels: " << p.end() << "s" << endl;
 	double loss = 0, tmp_loss;
 	cout << centers_vec.size() << endl;
@@ -81,16 +123,16 @@ void compute_sigma(const vector<VectorXd>& centers_vec,const int size, const int
 		for (int k = 0; k < kernels.size(); k++)
 		{
 			VectorXd vk(centers_vec[v] - kernels[k]);
-			cout << vk.dot(vk) << endl;
+			//cout << vk.dot(vk) << endl;
 			if (vk.dot(vk) < tmp_loss)
 				tmp_loss = vk.dot(vk);
 		}
-		if(tmp_loss<0.1)
-			cout << tmp_loss << endl;
+		/*if(tmp_loss<0.1)
+			cout << tmp_loss << endl;*/
 		loss += tmp_loss;
 	}
 	cout << "compute loss: " << p.end() <<"s"<< endl;
-	cout <<"sigma: "<< sigma << ", loss: " << loss << endl << endl;
+	cout << "sigma: "<< sigma << ", loss: " << loss << endl << endl;
 }
 
 void gaussian(const vector<Point>& centers)
@@ -114,10 +156,10 @@ void gaussian(const vector<Point>& centers)
 		centers_vec[i].normalize();
 	}
 	Performance p;
-	compute_sigma(centers_vec, 16, 1);
-	compute_sigma(centers_vec, 16, 2);
-	compute_sigma(centers_vec, 16, 3);
-	compute_sigma(centers_vec, 16, 4);
-	compute_sigma(centers_vec, 16, 5);
+	//for (double sigma = 1.5; sigma < 2.5; sigma+=0.1) // 1.8
+	//{
+	//	compute_sigma(centers_vec, 16, sigma);
+	//}
+	compute_sigma(centers_vec, 16, 1.8);
 	p.endAndPrint();
 }
--- demura/location.cpp
+++ demura/location.cpp
@@ -1,5 +1,5 @@
 #include "location.h"
-
+#include <fstream>
 using namespace std;
 using namespace cv;
 
@@ -11,22 +11,24 @@ vector<int> t_small({ -2, -1, 0, 1, 2 });
 vector<int> t_large({ -8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,-8 });
 void update(Point& p)
 {
-	Point tmp = p;
+	Point tmp(p);
 	for (int i = 0; i < t_update_small.size(); i++)
 	{
 		for (int j = 0; j < t_update_large.size(); j++)
 		{
-			/*if (img.at<Vec3b>(p.y, p.x)[0] < img.at<Vec3b>(tmp.y+ t_update[j], tmp.x+ t_update[i])[0])
+			if (img.at<Vec3b>(p.y, p.x)[0]
+				< img.at<Vec3b>(tmp.y + t_update_large[j], tmp.x + t_update_small[i])[0])
 			{
-				p.y = tmp.y + t_update[j];
-				p.x = tmp.x + t_update[i];
-			}*/
-			if (img.at<Vec3b>(p.y, p.x)[0] < 
+
+				p.y = tmp.y + t_update_large[j];
+				p.x = tmp.x + t_update_small[i];
+			}
+			/*if (img.at<Vec3b>(p.y, p.x)[0] < 
 				img.at<Vec3b>(p.y + t_update_large[j], p.x + t_update_small[i])[0])
 			{
 				p.y += t_update_large[j];
 				p.x += t_update_small[i];
-			}
+			}*/
 		}
 	}
 
@@ -75,6 +77,8 @@ void optimize_conner(vector<Point>& corner, int i)
 	}
 	corner[i+1] = tmp;
 
+	update(corner[i]);
+	update(corner[i+1]);
 	if ((corner[i].x - corner[i + 1].x)*(corner[i].x - corner[i + 1].x) +
 		(corner[i].y - corner[i + 1].y)*(corner[i].y - corner[i + 1].y) < 8)
 	{
@@ -188,48 +192,81 @@ void find_OLED_location(vector<Point>& centers)
 	}
 
 	// computer the count of points in one line
-	/*Mat img_copy = img;
-	float k = (1011 - 1009)*1.0 / (9603 - 337),
-		b = 1009 - 337 * k;
-	cout << k << " " << b << endl;
-	cout <<  - b / k << endl;
-	vector<Point> cnt_points;
-	for (int j = 0; j < img_copy.cols; j++)
-	{
-		if (img_copy.at<Vec3b>(k*j + b, j)[0] > low_limit)
+	/*{
+		Mat img_copy = img;
+		float k = (1011 - 1009)*1.0 / (9603 - 337),
+			b = 1009 - 337 * k;
+		cout << k << " " << b << endl;
+		cout << -b / k << endl;
+		vector<Point> cnt_points;
+		const int low_limit = 6;
+		ofstream out("./output/one_row.csv");
+		for (int j = 0; j < img_copy.cols; j++)
 		{
-			if (cnt_points.size() == 0
-				|| j - cnt_points[cnt_points.size() - 1].x > 5)
+			if (img_copy.at<Vec3b>(k*j + b, j)[0] > low_limit)
 			{
-				cnt_points.push_back(Point(j, k*j + b));
-				img_copy.at<Vec3b>(k*j + b, j) = Vec3b(0, 255, 0);
+				if (cnt_points.size() == 0
+					|| j - cnt_points[cnt_points.size() - 1].x > 5)
+				{
+					Point p(j, k*j + b), tmp(p);
+					for (int i = 0; i < t_small.size(); i++)
+					{
+						if (img.at<Vec3b>(p.y, p.x)[0] < img.at<Vec3b>(tmp.y, tmp.x + t_small[i])[0])
+						{
+							p.x = tmp.x + t_small[i];
+						}
+					}
+					update(p);
+					out << p.x << endl;
+					cnt_points.push_back(p);
+					img_copy.at<Vec3b>(p.y, p.x) = Vec3b(0, 255, 0);
+				}
 			}
 		}
-	}
-	cout << "size of point in one line: " << cnt_points.size() << endl;
-	imwrite("./output/G32_test.png", img_copy);*/
+		out.close();
+		cout << "size of point in one line: " << cnt_points.size() << endl;
+		imwrite("./output/G32_one_row.png", img_copy);
+	}*/
 
 	// computer the count of points in one bar
-	/*Mat img_copy = img;
-	float k = (6220 - 1008)*1.0 / (334 - 337),
-		b = 1008 - 337 * k;
-	cout << k << " " << b << endl;
-	cout <<  - b / k << endl;
-	vector<Point> cnt_points;
-	for (int y = 0; y < img_copy.rows; y++)
-	{
-		if (img_copy.at<Vec3b>(y, (y-b)/k)[0] > 10)
+	/*{
+		Mat img_copy = img;
+		//float k = (6220 - 1008)*1.0 / (334 - 337),
+		//	b = 1008 - 337 * k;
+		float k = (6223 - 1012)*1.0 / (329 - 332),
+			b = 1012 - 332 * k;
+		cout << k << " " << b << endl;
+		cout << -b / k << endl;
+		vector<Point> cnt_points;
+		ofstream out("./output/one_column.csv");
+		for (int y = 0; y < img_copy.rows; y++)
 		{
-			if (cnt_points.size() == 0
-				|| y - cnt_points[cnt_points.size() - 1].y > 6)
+			if (img_copy.at<Vec3b>(y, (y - b) / k)[0] > 7)
 			{
-				cnt_points.push_back(Point((y - b) / k, y));
-				img_copy.at<Vec3b>(y, (y - b) / k) = Vec3b(0, 255, 0);
+				if (cnt_points.size() == 0
+					|| y - cnt_points[cnt_points.size() - 1].y > 5)
+				{
+					//cnt_points.push_back(Point((y - b) / k, y));
+					//img_copy.at<Vec3b>(y, (y - b) / k) = Vec3b(0, 255, 0);
+					Point p((y - b) / k, y), tmp(p);
+					for (int i = 0; i < t_small.size(); i++)
+					{
+						if (img.at<Vec3b>(p.y, p.x)[0] < img.at<Vec3b>(tmp.y + t_small[i], tmp.x)[0])
+						{
+							p.y = tmp.y + t_small[i];
+						}
+					}
+					update(p);
+					out << p.y << endl;
+					cnt_points.push_back(p);
+					img_copy.at<Vec3b>(p.y, p.x) = Vec3b(0, 255, 0);
+				}
 			}
 		}
-	}
-	cout << "size of point in one line: " << cnt_points.size() << endl;
-	imwrite("./output/G32_test.png", img_copy);*/
+		out.close();
+		cout << "size of point in one line: " << cnt_points.size() << endl;
+		imwrite("./output/G32_one_column.png", img_copy * 8);
+	}*/
 
 	// draw corner
 	/*cout << i0 << " " << i1 << " " << j0 << " " << j1 << endl;
@@ -276,6 +313,8 @@ void find_OLED_location(vector<Point>& centers)
 	}
 	imwrite("./output/G32_corner.png", img_copy);*/
 	pp.endAndPrint();
+
+	//1
 
 //	{
 //		cout << "-----------------" << endl;
@@ -342,191 +381,151 @@ void find_OLED_location(vector<Point>& centers)
 //		imwrite("./output/G32_position.png", img_copy);
 //	}
 
+	// 2
+
+//	{
+//		//cout << "-----------------" << endl;
+//		//vector<Point> centers, line_head;
+//		vector<Point> line_head;
+//		{
+//			float k1 = (corner[1].y - corner[5].y)*1.0 / (corner[1].x - corner[5].x),
+//				b1 = corner[1].y - corner[1].x * k1,
+//				b2 = corner[0].y - corner[0].x * k1,
+//				add = (corner[5].y - corner[1].y) / 720.0;
+//			//cout << "add in y:"<< add << endl;
+//			for (float y1 = corner[1].y, y2 = corner[0].y;
+//				y1 <= corner[5].y, y2 <= corner[6].y; y1 += add, y2 += add)
+//			{
+//				line_head.push_back(Point((y2 - b2) / k1, y2));
+//				line_head.push_back(Point((y1 - b1) / k1, y1));
+//			}
+//		}
+//		{
+//			float k = (corner[0].y - corner[2].y)*1.0 / (corner[0].x - corner[2].x),
+//				add = (corner[2].x - corner[0].x) / 960.0;
+//			//cout << "add in x:" << add << endl;
+//#pragma parallel omp for
+//			for (auto p : line_head)
+//			{
+//				for (int x = p.x; x < corner[2].x; x += add)
+//				{
+//					float b = p.y - p.x*k;
+//					Point p(x, x*k + b);
+//					update(p);
+//					update(p);
+//					centers.push_back(p);
+//				}
+//			}
+//		}
+//		p.endAndPrint();
+//
+//		float real_value = 255.0 / 32;
+//		Mat img_copy = img * real_value;
+//		for (auto p : line_head)
+//		{
+//			img_copy.at<Vec3b>(p.y, p.x) = Vec3b(0, 255, 255);
+//		}
+//		for (auto p : centers)
+//		{
+//			img_copy.at<Vec3b>(p.y, p.x) = Vec3b(0, 0, 255);
+//		}
+//		//imwrite("./output/G32_position.png", img_copy);
+//	}
+
+	// 3
 	{
-		//cout << "-----------------" << endl;
-		//vector<Point> centers, line_head;
+		Mat img_copy = img;
+		const int low_limit = 5, initail_interval = 6;
 		vector<Point> line_head;
 		{
-			float k1 = (corner[1].y - corner[5].y)*1.0 / (corner[1].x - corner[5].x),
-				b1 = corner[1].y - corner[1].x * k1,
-				b2 = corner[0].y - corner[0].x * k1,
-				add = (corner[5].y - corner[1].y) / 720.0;
-			//cout << "add in y:"<< add << endl;
-			for (float y1 = corner[1].y, y2 = corner[0].y;
-				y1 <= corner[5].y, y2 <= corner[6].y; y1 += add, y2 += add)
+			float k = (corner[1].y - corner[5].y)*1.0 / (corner[1].x - corner[5].x),
+				b1 = corner[1].y - corner[1].x * k,
+				b2 = corner[0].y - corner[0].x * k;
+			for (float y = corner[1].y; y <= corner[5].y; y++)
 			{
-				line_head.push_back(Point((y2 - b2) / k1, y2));
-				line_head.push_back(Point((y1 - b1) / k1, y1));
-			}
-		}
-		{
-			float k = (corner[0].y - corner[2].y)*1.0 / (corner[0].x - corner[2].x),
-				add = (corner[2].x - corner[0].x) / 960.0;
-			//cout << "add in x:" << add << endl;
-#pragma parallel omp for
-			for (auto p : line_head)
-			{
-				for (int x = p.x; x < corner[2].x; x += add)
+				if (img_copy.at<Vec3b>(y, (y - b1) / k)[0] > low_limit &&
+					(line_head.size() == 0 || y - line_head[line_head.size() - 1].y > initail_interval))
 				{
-					float b = p.y - p.x*k;
-					Point p(x, x*k + b);
+					Point p((y - b1) / k, y), tmp(p);
+					for (int i = 0; i < t_small.size(); i++)
+					{
+						if (img.at<Vec3b>(p.y, p.x)[0] < img.at<Vec3b>(tmp.y + t_small[i], tmp.x)[0])
+						{
+							p.y = tmp.y + t_small[i];
+						}
+					}
 					update(p);
+					line_head.push_back(p);
+					img_copy.at<Vec3b>(p.y, p.x) = Vec3b(0, 255, 0);
+				}
+			}
+			int tmp = line_head.size();
+			for (float y = corner[0].y; y <= corner[4].y; y++)
+			{
+				if (img_copy.at<Vec3b>(y, (y - b2) / k)[0] > low_limit &&
+					(line_head.size() == tmp || y - line_head[line_head.size() - 1].y > initail_interval))
+				{
+					Point p((y - b2) / k, y), tmp(p);
+					for (int i = 0; i < t_small.size(); i++)
+					{
+						if (img.at<Vec3b>(p.y, p.x)[0] < img.at<Vec3b>(tmp.y + t_small[i], tmp.x)[0])
+						{
+							p.y = tmp.y + t_small[i];
+						}
+					}
 					update(p);
-					centers.push_back(p);
+					line_head.push_back(p);
+					img_copy.at<Vec3b>(p.y, p.x) = Vec3b(0, 255, 0);
+				}
+			}
+
+			k = (corner[0].y - corner[2].y)*1.0 / (corner[0].x - corner[2].x);
+			for (int hi = 0; hi < line_head.size(); hi++)
+			{
+				centers.push_back(line_head[hi]);
+				for (int x = line_head[hi].x++; x < corner[3].x; x++)
+				{
+					float b = line_head[hi].y - line_head[hi].x*k;
+					if (img_copy.at<Vec3b>(k*x + b, x)[0] > low_limit
+						&& x - centers[centers.size() - 1].x > initail_interval)
+					{
+						Point p(x, k*x + b), tmp(p);
+						for (int i = 0; i < t_small.size(); i++)
+						{
+							if (img.at<Vec3b>(p.y, p.x)[0] < img.at<Vec3b>(tmp.y, tmp.x + t_small[i])[0])
+							{
+								p.x = tmp.x + t_small[i];
+							}
+						}
+						update(p);
+						/*update(p);
+						update(p);*/
+						centers.push_back(p);
+					}
 				}
 			}
 		}
 		p.endAndPrint();
 
-		float real_value = 255.0 / 32;
-		Mat img_copy = img * real_value;
-		for (auto p : line_head)
+		/*int cnt = 0;
+		for (int i = 0; i < centers.size(); i++)
 		{
-			img_copy.at<Vec3b>(p.y, p.x) = Vec3b(0, 255, 255);
+			for (int j = 0; j < centers.size(); j++)
+				if (i != j && (abs(centers[j].x - centers[i].x)<=3) 
+					&& (abs(centers[i].y - centers[j].y)<=3))
+				{
+					if(abs(centers[i].y - centers[j].y) != 0)
+						cout << i<<","<<j<<" " << centers[i] <<","<<centers[j]<< endl;
+					cnt++;
+				}
 		}
+		cout <<"again cnt: "<< cnt << endl;*/
 		for (auto p : centers)
 		{
-			img_copy.at<Vec3b>(p.y, p.x) = Vec3b(0, 0, 255);
+			img_copy.at<Vec3b>(p.y, p.x) = Vec3b(0, 255, 0);
 		}
-		//imwrite("./output/G32_position.png", img_copy);
+		cout << "size of head points: " << line_head.size() << endl;
+		cout << 960*720*2 << ", size of points: " << centers.size() << endl;
+		imwrite("./output/G32_all.png", img_copy * 8);
 	}
 }
-
-//void find_OLED_location()
-//{
-//	Performance p;
-//	img = imread("G32.bmp");
-//	int i0 = -1, i1, j0 = -1, j1;
-//
-//	// find corner - not use
-//	//{
-//	//	Mat img_copy = img * 80;
-//	//	//GaussianBlur(img_copy, img_copy, Size(5, 5), 2, 2);
-//	//	/*for (int i = 0; i < img_copy.rows; i++)
-//	//	{
-//	//		for (int j = 0; j < img_copy.cols; j++)
-//	//		{
-//	//			if (img_copy.at<Vec3b>(i, j)[0] != 255
-//	//				|| img_copy.at<Vec3b>(i, j)[1] != 255
-//	//				|| img_copy.at<Vec3b>(i, j)[2] != 255)
-//	//			{
-//	//				img_copy.at<Vec3b>(i, j)[0] = 0;
-//	//				img_copy.at<Vec3b>(i, j)[1] = 0;
-//	//				img_copy.at<Vec3b>(i, j)[2] = 0;
-//	//			}
-//	//		}
-//	//	}*/
-//	//	int tmp = img_copy.cols / 2, i0_old = -1;
-//	//	for (int i = 0; i < img_copy.rows; i++)
-//	//	{
-//	//		if (img_copy.at<Vec3b>(i, tmp) == Vec3b(255, 255, 255)
-//	//			/*&& img_copy.at<Vec3b>(i, tmp + 1) == Vec3b(255, 255, 255)
-//	//			&& img_copy.at<Vec3b>(i, tmp - 1) == Vec3b(255, 255, 255)*/)
-//	//		{
-//	//			if (i0 == -1) i0 = i;
-//	//			i1 = i;
-//	//		}
-//	//	}
-//	//	tmp = img_copy.rows / 2;
-//	//	for (int j = 0; j < img_copy.cols; j++)
-//	//	{
-//	//		if (img_copy.at<Vec3b>(tmp, j) == Vec3b(255, 255, 255)
-//	//			/*&& img_copy.at<Vec3b>(tmp - 1, j) == Vec3b(255, 255, 255)
-//	//			&& img_copy.at<Vec3b>(tmp + 1, j) == Vec3b(255, 255, 255)*/)
-//	//		{
-//	//			if (j0 == -1) j0 = j;
-//	//			j1 = j;
-//	//		}
-//	//	}
-//	//	cout << i0 << " " << i1 << " " << j0 << " " << j1 << endl;
-//	//	line(img_copy, Point(j0, i0), Point(j1, i1), Scalar(0, 0, 255), 1);
-//	//	line(img_copy, Point(330, 1009), Point(9610, 6231), Scalar(255,0,0), 1);
-//	//	imwrite("./output/G32_cpoy.png", img_copy);
-//	//	//waitKey(0);
-//	//}
-//
-//	Performance pp;
-//	// find corner
-//	{
-//		const int low_limit = 5, initail_interval = 6;
-//		int tmp = img.cols / 2;
-//		for (int i = 0; i < img.rows; i++)
-//		{
-//			if (img.at<Vec3b>(i, tmp)[0] > low_limit
-//				|| img.at<Vec3b>(i, tmp - initail_interval)[0] > low_limit)
-//			{
-//				i0 = i;
-//				break;
-//			}
-//		}
-//		for (int i = img.rows - 1; i > 0; i--)
-//		{
-//			if (img.at<Vec3b>(i, tmp)[0] > low_limit
-//				|| img.at<Vec3b>(i, tmp - initail_interval)[0] > low_limit)
-//			{
-//				i1 = i;
-//				break;
-//			}
-//		}
-//		tmp = img.rows / 2;
-//		for (int j = 0; j < img.cols; j++)
-//		{
-//			if (img.at<Vec3b>(tmp, j)[0] > low_limit
-//				&&img.at<Vec3b>(tmp - initail_interval, j)[0] > low_limit)
-//			{
-//				j0 = j;
-//				break;
-//			}
-//		}
-//		for (int j = img.cols - 1; j > 0; j--)
-//		{
-//			if (img.at<Vec3b>(tmp, j)[0] > low_limit
-//				&&img.at<Vec3b>(tmp - initail_interval, j)[0] > low_limit)
-//			{
-//				j1 = j;
-//				break;
-//			}
-//		}
-//	}
-//	pp.endAndPrint();
-//
-//	{
-//		cout << "-----------------" << endl;
-//		vector<Point> centers;
-//		float real_value = 255.0 / 32,
-//			addi = 7,
-//			addj = 9;
-//		Mat img_copy = img * real_value;
-//#pragma parallel omp for
-//		for (float i = i0; i <= i1; i += addi)
-//			//int i = i0;
-//		{
-//			//cout << (int)((i-i0)/addi + 1) % 2 * addj / 2 << endl;
-//			//for (float j = j0 + (int)((i - i0) / addi + 1) % 2 * addj; j <= j1; j += addj)
-//			//for (float j = corner[1].x; j <= corner[1].x; j += addj)
-//			for (float j = j0; j <= j1; j += addj)
-//			{
-//				Point p(j, i);
-//				img_copy.at<Vec3b>(i, j) = Vec3b(0, 255, 255);
-//				update(p);
-//				//update(p);
-//				centers.push_back(p);
-//			}
-//		}
-//		p.endAndPrint();
-//
-//		cout << "cornor index : " << i0 << " " << j0 << " " << i1 << " " << j1 << endl;
-//		cout << "internal: " << addi << " " << addj << endl;
-//		cout << "point size: " << centers.size() << endl;
-//		/*img_copy.at<Vec3b>(i0, j0) = Vec3b(0, 255, 0);
-//		img_copy.at<Vec3b>(i1, j1) = Vec3b(0, 255, 0);
-//		img_copy.at<Vec3b>(i1, j0) = Vec3b(0, 255, 0);
-//		img_copy.at<Vec3b>(i0, j1) = Vec3b(0, 255, 0);*/
-//		for (auto p : centers)
-//		{
-//			img_copy.at<Vec3b>(p.y, p.x) = Vec3b(0, 0, 255);
-//		}
-//
-//		imwrite("./output/G32_position.png", img_copy);
-//	}
-//}
